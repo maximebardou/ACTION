f3de6e837c6b2e69cf28ce26c0d6deaafa7953f2
maximebardou <49574637+maximebardou@users.noreply.github.com>
2019-05-01 18:37:42 +0200

    Add files via upload

---
+++ data/Recyclage_4.js
@@ -0,0 +1,1 @@
+var json_Recyclage_4 = {"type":"FeatureCollection","name":"Recyclage_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null},"geometry":{"type":"Point","coordinates":[3.519834076562615,50.548991004170212]}}]}
